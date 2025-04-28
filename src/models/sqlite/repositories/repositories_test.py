26e4036e342d0c11b1b87e942c0b5e3fc4842ea4
caioccassiano <caioccassiano@icloud.com>
2025-04-28 16:01:29 +0200

    Adding people repository and testing adding data to daatabase

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,15 +1,43 @@
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repositoy import PetsRepository
+from .people_repository import PeopleRepository
+import pytest
 
-db_connection_handler.connect_to_db()
+#db_connection_handler.connect_to_db()
 
+@pytest.mark.skip(reason="Interacao com o banco")
 def test_list_pets():
   repo = PetsRepository(db_connection_handler)
   response = repo.list_pets()
   print()
   print (response)
-  
+
+
+@pytest.mark.skip(reason="Interacao com o banco")  
 def test_delete_pets():
   name = "shrek"
   repo = PetsRepository(db_connection_handler)
   repo.delete_pets(name=name)
+
+
+@pytest.mark.skip(reason="Interacao com o banco")
+def test_insert_people():
+  first_name = "test name"
+  last_name = "test last"
+  age = 65
+  pet_id = 2
+  repo = PeopleRepository(db_connection_handler)
+  repo.insert_people(first_name, last_name, age, pet_id)
+
+@pytest.mark.skip(reason="Interacao com o banco")
+def test_get_person():
+  person_id = 1
+  repo = PeopleRepository(db_connection_handler)
+  response = repo.get_person(person_id=person_id)
+  print()
+  print(response)
+  print(response.pet_name)
+
+
+
+
